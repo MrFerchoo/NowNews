ad60b4acf0959ed331f9215cc3f0d70ace524e3d
VirkovFM <vfm72673@udelasalle.edu.mx>
2022-09-30 15:07:42 -0500

    first commit

---
+++ app/src/main/java/Entidades/Evento.kt
@@ -0,0 +1,7 @@
+package Entidades
+
+class Evento (ID_EVENTO:Int, DESCRIPCION_EVENTO:String,DIA_EVENTO:String){
+    var iD_EVENTO = ID_EVENTO
+    var descripcioN_EVENTO = DESCRIPCION_EVENTO
+    var diA_EVENTO = DIA_EVENTO
+}
